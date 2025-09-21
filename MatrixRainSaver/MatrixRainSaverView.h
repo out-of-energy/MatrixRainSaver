8badd0865a14b11c3daa784b0be1102de5cefb40
kk <shinear@outlook.com>
2025-09-21 23:19:03 +0800

    Initial Commit

---
+++ MatrixRainSaver/MatrixRainSaverView.h
@@ -0,0 +1,12 @@
+//
+//  MatrixRainSaverView.h
+//  MatrixRainSaver
+//
+//  Created by 张凯 on 2025/9/21.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface MatrixRainSaverView : ScreenSaverView
+
+@end
---
+++ MatrixRainSaver/MatrixRainSaverView.m
@@ -0,0 +1,51 @@
+//
+//  MatrixRainSaverView.m
+//  MatrixRainSaver
+//
+//  Created by 张凯 on 2025/9/21.
+//
+
+#import "MatrixRainSaverView.h"
+
+@implementation MatrixRainSaverView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
